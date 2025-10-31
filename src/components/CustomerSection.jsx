4a3cc1fad7333ce591d69288c3a2b205178746ea
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-31 20:21:43 +0530

    Update CustomerSection.jsx

--- src/components/CustomerSection.jsx
+++ src/components/CustomerSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';               
+import React, { useState, useRef } from 'react';                
 import { HiArrowLeft, HiArrowRight } from 'react-icons/hi';
 import { BsPlayFill, BsPauseFill } from 'react-icons/bs';         
  
