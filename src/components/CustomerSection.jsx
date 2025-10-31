135747e3d70fe18027b613c494990ca82c7cf442
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-31 20:26:11 +0530

    Update CustomerSection.jsx

--- src/components/CustomerSection.jsx
+++ src/components/CustomerSection.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import { HiArrowLeft, HiArrowRight } from 'react-icons/hi'; 
 import { BsPlayFill, BsPauseFill } from 'react-icons/bs';                    
    
-const customerData = [
+const customerData = [ 
   {
     id: 'techstart',  
     logo: 'https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66ec7ff7808fef2aeecfff1b_Outliant.svg',
