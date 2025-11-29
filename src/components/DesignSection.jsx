64764d54e1b8de53f3b8a8fc770cae2dc26a50f0
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-29 19:43:05 +0530

    Update DesignSection.jsx

--- src/components/DesignSection.jsx
+++ src/components/DesignSection.jsx
@@ -4,7 +4,7 @@ import { HiArrowRight } from 'react-icons/hi';
 import { BsPlayFill, BsPauseFill } from 'react-icons/bs'; 
   
 const tabsData = [   
-  {  
+  {   
     id: 'tab1', 
     title: 'Design without limits',
     subtitle: 'CodeTutor puts the power of code into a visual canvas so every team can create stunning websites quickly — and extend the power of their work with custom code.',
