4c704566250d83c7ee9e67c773a180de85cebdd4
ABISHEK ANTONY <abishek8673@gmail.com>
2025-12-16 17:17:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import CompanyLogo from './components/CompanyLogo' 
+import CompanyLogo from './components/CompanyLogo'  
 import FeaturesSection from './components/FeaturesSection' 
 import DesignSection from './components/DesignSection'
 import CustomerSection from './components/CustomerSection'
