1cbf609d14ad30a697b6fae054430db5f5f2a0d3
ABISHEK ANTONY <abishek8673@gmail.com>
2026-03-02 17:56:40 +0530

    Update FeaturesSection.jsx

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -7,7 +7,7 @@ const FeaturesSection = () => {
       title: "Design and build",     
       description: "Designers can take control of HTML, CSS, and JavaScript in a visual canvas — while marketers can work with pre-made, design-approved building blocks.",
       link: "#design-build"
-    }, 
+    },  
     {
       icon: <FiUploadCloud className="w-8 h-8" />,
       title: "Publish and edit",
